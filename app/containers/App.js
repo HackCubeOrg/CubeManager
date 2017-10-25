afd62f171cbf160d1be8a1abbf440bcf11268271
Sebastian Czarnecki <sebastian.czarnecki@sympa.com>
2017-10-25 17:07:41 +0200

    Add material-ui

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,17 +1,61 @@
 // @flow
 import React, { Component } from 'react';
 import type { Children } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import FontIcon from 'material-ui/FontIcon';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+
+import AppBar from 'material-ui/AppBar';
+
+
+const recentsIcon = <FontIcon className="material-icons">desktop</FontIcon>;
+const favoritesIcon = <FontIcon className="material-icons">manage</FontIcon>;
+const nearbyIcon = <FontIcon className="material-icons">contact</FontIcon>;
 
 export default class App extends Component {
   props: {
     children: Children
   };
 
+  state = {
+    selectedIndex: 0,
+  };
+
+  select = (index) => this.setState({ selectedIndex: index });
+
   render() {
     return (
-      <div>
-        {this.props.children}
-      </div>
+      <MuiThemeProvider>
+        <div className="container">
+          <AppBar title="Hack Cube" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+          <div className="main-content">
+            {this.props.children}
+          </div>
+          <footer className="footer">
+            <Paper>
+              <BottomNavigation selectedIndex={this.state.selectedIndex}>
+                <BottomNavigationItem
+                  label="Pulpit"
+                  icon={recentsIcon}
+                  onClick={() => this.select(0)}
+                />
+                <BottomNavigationItem
+                  label="Zarządzaj"
+                  icon={favoritesIcon}
+                  onClick={() => this.select(1)}
+                />
+                <BottomNavigationItem
+                  label="Kontakt"
+                  icon={nearbyIcon}
+                  onClick={() => this.select(2)}
+                />
+              </BottomNavigation>
+            </Paper>
+          </footer>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,16 +1,29 @@
 // @flow
 import React, { Component } from 'react';
-import { Window, TitleBar, Text } from 'react-desktop/macOs';
+
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+
+import Paper from 'material-ui/Paper';
 
 import Home from '../components/Home';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <Window chrome height="300px" padding="10px">
-        <TitleBar title="untitled text 5" controls />
-        <Text>Hello World</Text>
-      </Window>
+      <div className="desktop-wrapper">
+        <Paper className="desktop-panel desktop-left" zDepth={1}>
+          dasd
+        </Paper>
+        <Paper className="desktop-panel desktop-right" zDepth={1}>
+          adsd
+        </Paper>
+      </div>
     );
   }
 }
